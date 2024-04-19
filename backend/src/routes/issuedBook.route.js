a80e25f042cebac8fc4f99f8d7f9a0eee6d6bb19
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-04-19 18:14:10 +0530

    add issueBook routes.js

---
+++ backend/src/routes/issuedBook.route.js
@@ -0,0 +1,23 @@
+
+import express from 'express'
+import { 
+    getAllLonedBooks, 
+    getDueSoonLone, 
+    getLoanDetail, 
+    getLoneHistoryOfUser, 
+    getOverDueLone, 
+    reserveBook, 
+    returnBook 
+} from '../controllers/issuedBook.controller.js';
+
+const router = express.Router();
+
+router.route('/reserve-book').post(verifyUser, reserveBook)
+router.route('/return-book').post(verifyUser, returnBook)
+router.route('/get-loan-detail/:bookId').post(verifyUser, getLoanDetail)
+router.route('/get-all-loned-book').post(verifyUser,getAllLonedBooks)
+router.route('/get-loned-history/:userId').post(verifyUser, getLoneHistoryOfUser)
+router.route('/get-overdue-loan/:userId').post(verifyUser, getOverDueLone)
+router.route('/get-due-soon-loand/:userId').post(verifyUser, getDueSoonLone)
+
+export default router

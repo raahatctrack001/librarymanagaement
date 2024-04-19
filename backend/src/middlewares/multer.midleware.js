073875cd8b38de3ee379637dd2f4fcef5c003915
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-04-19 11:31:11 +0530

    add multer to parse multipart/form data

---
+++ backend/src/middlewares/multer.midleware.js
@@ -0,0 +1,15 @@
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    },
+    destination: function (req, file, cb) {
+      // console.log("inside multer:")
+      cb(null, 'public/files')
+    }
+  })
+  
+  const upload = multer({ storage: storage })
+  export default upload;

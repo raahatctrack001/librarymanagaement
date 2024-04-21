a4e74497a6fbd2003724761291afee9ec56e14cb
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-04-21 12:47:11 +0530

    add header section UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import SignIn from "./pages/SignIn"
+import Register from "./pages/Register"
+import About from "./pages/About"
+import Projects from "./pages/Projects"
 
 function App() {
 
@@ -9,8 +13,11 @@ function App() {
     <BrowserRouter>
       <Header />
         <Routes>
-          <Route path="/" element= {<Home />} />
-    
+          <Route path="/" element = {<Home />} />
+          <Route path="/sign-in" element = {<SignIn />} />
+          <Route path="/register" element = {<Register />} />
+          <Route path="/about" element = {<About />} />
+          <Route path="/project" element = {<Projects />} />
         </Routes>
       <Footer />
     </BrowserRouter>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,40 @@
-cd import React from 'react'
-
+// Header.jsx
+import React from 'react';
+import { FaMoon, FaSearch } from 'react-icons/fa'
+import { TextInput } from 'flowbite-react'
+import { Link } from 'react-router-dom';
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="flex flex-col md:flex-row items-center justify-between px-4 py-5 bg-gray-800 text-white">
+      <div className="flex my-auto items-center">
+        <div className="text-lg font-bold">Library Management System</div>
+        <div className="md:ml-4 mt-4 md:mt-0">
+          <TextInput
+            type="text"
+            placeholder="Search..."
+            className="px-3 py-1 w-full md:w-auto border border-gray-600 rounded-lg focus:outline-none focus:border-blue-500"
+          />
+        </div>
+      </div>
+      <div className="flex items-center gap-4 mt-4 md:mt-0 md:ml-4">
+        <button className="h-12 w-14 px-5 bg-blue-500 text-white rounded-lg focus:outline-none hover:bg-blue-600">
+          <FaMoon />
+        </button>
+        <Link to={'/sign-in'}>
+          <button className="h-12 w-18 px-4 bg-green-500 text-white rounded-lg focus:outline-none hover:bg-green-600">
+            Sign In
+          </button>
+        </Link>
+        <nav className="text-lg font-semibold">
+          <ul className="flex space-x-4">
+            <li><Link  to={"/"} className="hover:text-blue-300">Home</Link></li>
+            <li><Link  to={"/about"} className="hover:text-blue-300">About</Link></li>
+            <li><Link  to={"/projects"} className="hover:text-blue-300">Projects</Link></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
